46eaff4b7759f462baeb6d54d83a3be7c588415a
Alpha <alphagma@gmail.com>
2014-12-28 21:36:47 -0600

    Added jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+var TEST_SRC_FILES = ['test/**/*.js'];
+var LIB_SRC_FILES = ['lib/**'];
 
-gulp.task('mocha', function() {
-  return gulp.src(['test/*.js'], { read: false })
+gulp.task('default', ['mocha']);
+
+gulp.task('mocha', ['lint'], function() {
+  return gulp.src(TEST_SRC_FILES, { read: false })
     .pipe(mocha({ reporter: 'spec' }))
     .on('error', gutil.log);
 });
 
 gulp.task('watch-mocha', function() {
-  gulp.watch(['lib/**', 'test/**'], ['mocha']);
+  gulp.watch([].concat(TEST_SRC_FILES).concat(LIB_SRC_FILES), ['mocha']);
+});
+
+gulp.task('lint', function() {
+  gulp.src(LIB_SRC_FILES)
+    .pipe(jshint())
+    .pipe(jshint.reporter('cool-reporter'))
+    .pipe(jshint.reporter('default'), { verbose: true });
 });
