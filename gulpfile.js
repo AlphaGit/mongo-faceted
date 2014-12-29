acf51fa170ecccd31195ce8512776eb71b0fca9b
Alpha <alphagma@gmail.com>
2014-12-28 21:48:40 -0600

    Added jslint notifications

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,31 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
+var notify = require('gulp-notify');
 
-var TEST_SRC_FILES = ['test/**/*.js'];
-var LIB_SRC_FILES = ['lib/**'];
+var TEST_SRC_FILES = 'test/**/*.js';
+var LIB_SRC_FILES = 'lib/**/*.js';
 
-gulp.task('default', ['mocha']);
+gulp.task('default', ['lint', 'mocha']);
 
-gulp.task('mocha', ['lint'], function() {
+gulp.task('mocha', function() {
   return gulp.src(TEST_SRC_FILES, { read: false })
     .pipe(mocha({ reporter: 'spec' }))
     .on('error', gutil.log);
 });
 
 gulp.task('watch-mocha', function() {
-  gulp.watch([].concat(TEST_SRC_FILES).concat(LIB_SRC_FILES), ['mocha']);
+  gulp.watch([TEST_SRC_FILES, LIB_SRC_FILES], ['lint', 'mocha']);
 });
 
 gulp.task('lint', function() {
   gulp.src(LIB_SRC_FILES)
     .pipe(jshint())
     .pipe(jshint.reporter('cool-reporter'))
-    .pipe(jshint.reporter('default'), { verbose: true });
+    .pipe(jshint.reporter('fail'))
+    .pipe(notify({
+      title: 'JSHint',
+      message: 'JSHint passed.',
+      onLast: true
+    }));
 });
