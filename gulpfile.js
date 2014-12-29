4e57703d7b2c25fbf4c62158ed972fe136c46c77
Alpha <alphagma@gmail.com>
2014-12-28 23:06:16 -0600

    Added JSCS

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
-var notify = require('gulp-notify');
+var jscs = require('gulp-jscs');
 
 var TEST_SRC_FILES = 'test/**/*.js';
 var LIB_SRC_FILES = 'lib/**/*.js';
 
-gulp.task('default', ['lint', 'mocha']);
+gulp.task('default', ['lint', 'jscs', 'mocha']);
 
 gulp.task('mocha', function() {
   return gulp.src(TEST_SRC_FILES, { read: false })
@@ -16,17 +16,17 @@ gulp.task('mocha', function() {
 });
 
 gulp.task('watch-mocha', function() {
-  gulp.watch([TEST_SRC_FILES, LIB_SRC_FILES], ['lint', 'mocha']);
+  gulp.watch([TEST_SRC_FILES, LIB_SRC_FILES], ['lint', 'jscs', 'mocha']);
 });
 
 gulp.task('lint', function() {
   gulp.src(LIB_SRC_FILES)
     .pipe(jshint())
     .pipe(jshint.reporter('cool-reporter'))
-    .pipe(jshint.reporter('fail'))
-    .pipe(notify({
-      title: 'JSHint',
-      message: 'JSHint passed.',
-      onLast: true
-    }));
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('jscs', function() {
+  gulp.src(LIB_SRC_FILES)
+    .pipe(jscs());
 });
